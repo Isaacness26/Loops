78af9e59820ab1cb0b4b7bf5d16e1393287e1384
isaacness26 <isaacness26@g.crookcountyschools.org>
2025-10-17 15:02:07 -0700

    added FOR and WHILE loops in javascript

---
+++ script.js
@@ -0,0 +1,22 @@
+let i = 0
+while(i<15){
+    console.log(i)
+    i++
+}
+
+const subjects = ['math', 'science', 'history', 'english', 'spanish']
+let x = 0
+while(x<5){
+    console.log(subjects[x])
+    x++
+}
+
+
+for(let y = 1; y < 10; y = y + 2){
+console.log(y)
+}
+
+const movies = ['Star Wars', 'Lord of the Rings', 'Avengers', 'Gladiator', '13 Hours', 'Terminator', 'Forrest Gump', 'Saving Private Ryan', 'The Matrix']
+for (let t = 0; t < 9; t = t + 1){
+console.log(movies[t])
+}
